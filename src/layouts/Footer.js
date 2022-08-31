6125aa4cf9e878cb61a9e52d8633bffca4b55fd7
Alexandre-st <alexandre.saintprix@gmail.com>
2022-08-31 17:08:02 +0200

    Add footer with final design, need to do the router - WIP -

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -4,15 +4,17 @@ import Arrow from '../assets/icons/icon-arrow.svg';
 const Footer = () => {
   return ( 
     <footer className="footer">
-      <div className="footer-container container">
-        <div className="footer-img">
-          <img src={ArcLogo} alt="Arc" />
+      <div className="footer-container container-img">
+        <div className="footer-content">
+          <div className="footer-img">
+            <img src={ArcLogo} alt="Arc" />
+          </div>
+          <ul className="footer-list">
+            <li>Portfolio</li>
+            <li>About Us</li>
+            <li>Contact</li>
+          </ul>
         </div>
-        <ul className="footer-content">
-          <li>Portfolio</li>
-          <li>About Us</li>
-          <li>Contact</li>
-        </ul>
         <div className="footer-button button" role="button">
           <p>See Our Portfolio</p>
           <img src={Arrow} alt="Arrow link" />
