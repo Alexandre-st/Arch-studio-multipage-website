bf1897f663ceedc8238fdae1bb56035157a134a9
Alexandre-st <alexandre.saintprix@gmail.com>
2022-09-15 14:54:27 +0200

    Add the configuration for the map layer

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,13 +1,33 @@
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer,
+  //  Marker, Popup 
+  } from 'react-leaflet';
 
 const Map = () => {
+
+  // const position = [51.505, -0.09];
+
   return (
     <div className="location">
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <MapContainer 
+        center={[51.505, -0.09]} 
+        zoom={13} 
+        scrollWheelZoom={false}
+        doubleClickZoom={false}
+        zoomControl={false}
+        attributionControl={false}
+        keyboard={false}
+        touchZoom={false}
+        dragging={false}
+      >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+        {/* <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker> */}
       </MapContainer>
     </div>
   )
