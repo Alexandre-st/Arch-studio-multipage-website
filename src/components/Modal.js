2973cd5d7ae7034135581a47f5b255b8e6864d1f
Alexandre-st <alexandre.saintprix@gmail.com>
2022-11-08 18:46:34 +0100

    Add the final css modal, and fix a margin error in the header

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,8 +1,12 @@
 import { useRef } from 'react';
 import ReactDOM from 'react-dom';
 
+import Close from '../assets/icons/icon-close.svg';
+
 export const Modal = ({ setShowModal }) => {
   const modalRef = useRef();
+
+  const closeImg = <img className="modal-close" src={Close} alt="To close the modal" />
 
   const closeModal = ( evt ) => {
     if (evt.target === modalRef.current) {
@@ -13,9 +17,12 @@ export const Modal = ({ setShowModal }) => {
   return ReactDOM.createPortal(
     <div className="modal" ref={modalRef} onClick={closeModal}>
       <div className="modal-container">
-        <h6>Thank you for contacting us!</h6>
+        <h6 className="small-title">Thank you for contacting us !</h6>
+        <button className="modal-button" onClick={() => setShowModal(false)}>
+          {closeImg}
+        </button>
       </div>
     </div>,
-    document.getElementById('portal')
+    document.getElementById("portal")
   );
 };
