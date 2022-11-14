26504cd445f135d7574589afb8f0b497939e8884
Alexandre-st <alexandre.saintprix@gmail.com>
2022-11-14 18:42:23 +0100

    Begin the hover effects for the leaders contact part

---
+++ src/data/leaders.js
@@ -0,0 +1,35 @@
+import Jake from '../assets/about/desktop/avatar-jake.jpg';
+import Thompson from '../assets/about/desktop/avatar-thompson.jpg';
+import Jackson from '../assets/about/desktop/avatar-jackson.jpg';
+import Maria from '../assets/about/desktop/avatar-maria.jpg';
+
+export const leaders = [
+  {
+    name: "Jake Richards",
+    job: "Chief Architect",
+    image: Jake,
+    linkedin: "#",
+    twitter: "#",
+  },
+  {
+    name: "Thompson Smith",
+    job: "Head of Finance",
+    image: Thompson,
+    linkedin: "#",
+    twitter: "#",
+  },
+  {
+    name: "Jackson Rourke",
+    job: "Lead Designer",
+    image: Jackson,
+    linkedin: "#",
+    twitter: "#",
+  },
+  {
+    name: "Maria Simpson",
+    job: "Senior Architect",
+    image: Maria,
+    linkedin: "#",
+    twitter: "#",
+  },
+];
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,22 +1,27 @@
-import MHero from '../assets/about/mobile/image-hero.jpg';
-import THero from '../assets/about/tablet/image-hero.jpg';
-import DHero from '../assets/about/desktop/image-hero.jpg';
-import Jake from '../assets/about/desktop/avatar-jake.jpg';
-import Thompson from '../assets/about/desktop/avatar-thompson.jpg';
-import Jackson from '../assets/about/desktop/avatar-jackson.jpg';
-import Maria from '../assets/about/desktop/avatar-maria.jpg';
-import Heritage from '../assets/about/desktop/image-heritage.jpg';
+import { Link } from "react-router-dom";
+
+import MHero from "../assets/about/mobile/image-hero.jpg";
+import THero from "../assets/about/tablet/image-hero.jpg";
+import DHero from "../assets/about/desktop/image-hero.jpg";
+// import Jake from '../assets/about/desktop/avatar-jake.jpg';
+// import Thompson from '../assets/about/desktop/avatar-thompson.jpg';
+// import Jackson from '../assets/about/desktop/avatar-jackson.jpg';
+// import Maria from '../assets/about/desktop/avatar-maria.jpg';
+import Linkedin from "../assets/icons/icon-linkedin.svg";
+import Twitter from "../assets/icons/icon-twitter.svg";
+import Heritage from "../assets/about/desktop/image-heritage.jpg";
+import { leaders } from "../data/leaders";
 
 const About = () => {
-  return ( 
+  return (
     <main>
       <section className="about-hero">
         <div className="container-img">
           <div className="about-hero-content">
             <div className="about-hero-content-img">
               <picture>
-                <source srcSet={DHero} media='(min-width: 1024px)' />
-                <source srcSet={THero} media='(min-width: 768px)' />
+                <source srcSet={DHero} media="(min-width: 1024px)" />
+                <source srcSet={THero} media="(min-width: 768px)" />
                 <img src={MHero} alt="Professional working on his device" />
               </picture>
             </div>
@@ -28,7 +33,10 @@ const About = () => {
                   <div className="lil-border hero-border" />
                   <h1 className="mid-title">Your team of professionals</h1>
                   <p className="text text-grey">
-                    Our small team of world-class professionals will work with you every step of the way. Strong relationships are at the core of everything we do. This extends to the relationship our projects have with their surroundings.
+                    Our small team of world-class professionals will work with
+                    you every step of the way. Strong relationships are at the
+                    core of everything we do. This extends to the relationship
+                    our projects have with their surroundings.
                   </p>
                 </div>
               </div>
@@ -42,32 +50,71 @@ const About = () => {
           <div className="right-content">
             <div className="lil-border heritage-border" />
             <div className="heritage-content">
-              <h2 className="mid-title">Our<br />Heritage</h2>
+              <h2 className="mid-title">
+                Our
+                <br />
+                Heritage
+              </h2>
               <p className="text text-grey">
-              Founded in 2007, we started as a trio of architects. Our complimentary skills and relentless attention to detail turned Arch into one of the most sought after boutique firms in the country.<br /><br />
-              Speciliazing in Urban Design allowed us to focus on creating exceptional structures that live in harmony with their surroundings. We consider every detail from every surrounding element to inform our designs.<br /><br />
-              Our small team of world-class professionals provides input on every project.
+                Founded in 2007, we started as a trio of architects. Our
+                complimentary skills and relentless attention to detail turned
+                Arch into one of the most sought after boutique firms in the
+                country.
+                <br />
+                <br />
+                Speciliazing in Urban Design allowed us to focus on creating
+                exceptional structures that live in harmony with their
+                surroundings. We consider every detail from every surrounding
+                element to inform our designs.
+                <br />
+                <br />
+                Our small team of world-class professionals provides input on
+                every project.
               </p>
             </div>
           </div>
           <div className="left-content">
-            <img className="heritage-img" src={Heritage} alt='Heritage building'  />
+            <img
+              className="heritage-img"
+              src={Heritage}
+              alt="Heritage building"
+            />
           </div>
         </div>
       </section>
 
       <section>
         <div className="leaders container">
-          <h3 className="mid-title">The<br />Leaders</h3>
+          <h3 className="mid-title">
+            The
+            <br />
+            Leaders
+          </h3>
           <div className="leaders-container">
-            <div className="leaders-content">
-              <img className="leaders-content-img" src={Jake} alt="Jake Richards" />
-              <div className="leaders-content-text">
-                <h5 className="lil-title">Jake Richards</h5>
-                <p className="text text-grey">Chief Architect</p>
-              </div>
-            </div>
-            <div className="leaders-content">
+            {leaders.map((leader, index) => {
+              return (
+                <div className="leaders-content" key={index}>
+                  <img
+                    className="leaders-content-img"
+                    src={leader.image}
+                    alt={leader.name}
+                  />
+                  <div className="leaders-content-text">
+                    <h5 className="lil-title">{leader.name}</h5>
+                    <p className="text text-grey">{leader.job}</p>
+                  </div>
+                  <div className="leaders-link">
+                    <Link className="leaders-link--link" to={leader.linkedin}>
+                      <img src={Linkedin} alt="Linkedin link" />
+                    </Link>
+                    <Link className="leaders-link--link" to={leader.t}>
+                      <img src={Twitter} alt="Twitter link" />
+                    </Link>
+                  </div>
+                </div>
+              );
+            })}
+            {/* <div className="leaders-content">
               <img className="leaders-content-img" src={Thompson} alt="Thompson Smith" />
               <div className="leaders-content-text">
                 <h5 className="lil-title">Thompson Smith</h5>
@@ -87,12 +134,12 @@ const About = () => {
                 <h5 className="lil-title">Maria Simpson</h5>
                 <p className="text text-grey">Senior Architect</p>
               </div>
-            </div>
+            </div> */}
           </div>
         </div>
       </section>
     </main>
   );
-}
- 
+};
+
 export default About;
